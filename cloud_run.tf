8349b1e5024167b48e34230c8e9582265119bf16
Mike Beaumont <mjboamail@gmail.com>
2025-06-20 14:15:08 +0200

    feat: upgrade to v1.4.0

--- cloud_run.tf
+++ cloud_run.tf
@@ -91,7 +91,7 @@ resource "google_cloud_run_v2_service" "this" {
 
     containers {
       name  = "pocket-id"
-      image = "${local.registry_uri}/pocket-id/pocket-id:v1.2.0"
+      image = "${local.registry_uri}/pocket-id/pocket-id:v1.4.0"
       ports {
         container_port = 8080
       }
